5e9de19bef6702481d2a859cc24f17371e93ddea
Guilherme Silva <guisilvaoliver55@gmail.com>
2024-09-18 19:10:47 -0300

    Deixando o jogo mais facil

--- js-curso-2-aula_2/app.js
+++ js-curso-2-aula_2/app.js
@@ -1,5 +1,5 @@
 let = listaDeNumerosSorteados = [];
-let numeroLimete = 100
+let numeroLimete = 50
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1
 function exibirTextoNaTela(tag, texto) {
